074324af0e63326b07c8535411332537f2089f80
aditya <rautaditya213@gmail.com>
2026-06-02 12:17:53 +0530

    making deplyomenet ready

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,11 @@ app.use(cors({
     credentials: true
 }))
 
+
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
